e14e95b915e767f834885d65b0825dd44d2dd804
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-12 10:09:12 +0100

    done: Screen setup

---
+++ main.py
@@ -0,0 +1,8 @@
+# import turtle module
+import turtle
+
+# Screen Setup
+wind = turtle.Screen()
+wind.title("Ping Pong by Youssef nasrallah")
+wind.bgcolor("black")
+wind.setup(width=800, height=600)
